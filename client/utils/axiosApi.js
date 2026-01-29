c86f6e36ec686fafaa0bbc79e9807795b7137d36
projectblurimedia <projectblurimedia@gmail.com>
2026-01-29 10:13:17 +0530

    frontend

--- client/utils/axiosApi.js
+++ client/utils/axiosApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosApi = axios.create({
-  baseURL: 'http://192.168.31.232:8000/server', 
+  // baseURL: 'http://192.168.31.232:8000/server', 
+  baseURL: 'https://campusphere-app-backend.onrender.com/server', 
   headers: {
     'Content-Type': 'application/json',
   }

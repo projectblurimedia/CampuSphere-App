d3845033b25dd9b7be74672fa18d369f518777f7
projectblurimedia <projectblurimedia@gmail.com>
2026-02-17 08:44:57 +0530

    Cashflow

--- client/components/cashflow/quick-actions.jsx
+++ client/components/cashflow/quick-actions.jsx
@@ -136,7 +136,7 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.1,
     shadowRadius: 8,
-    elevation: 2,
+    elevation: 1,
   },
   actionIcon: {
     width: 52,

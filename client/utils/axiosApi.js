103e262da4b63c4a32091e8c6106a199970e954e
projectblurimedia <projectblurimedia@gmail.com>
2026-04-27 15:33:45 +0530

    Update Successfull

--- client/utils/axiosApi.js
+++ client/utils/axiosApi.js
@@ -3,9 +3,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 // Create axios instance
 const axiosApi = axios.create({
-  baseURL: 'http://192.168.31.232:8080/api', 
+  // baseURL: 'http://192.168.31.232:8080/api', 
   // baseURL: 'https://campusphere-app-api.onrender.com/api', 
-  // baseURL: 'https://campusphere-app-api-j50e.onrender.com/api', 
+  baseURL: 'https://campusphere-app-api-j50e.onrender.com/api', 
   timeout: 50000,
   headers: {
     'Content-Type': 'application/json',
